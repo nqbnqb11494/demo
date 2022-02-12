7b986125288a6d43903effaec041cc261be18004
nqbnq <nqbnq@NQB>
2022-02-12 16:39:20 +0800

    first commit

---
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	@RequestMapping(value = "/")
+    public String home(Model model) {
+        return "index";
+    }
+}
